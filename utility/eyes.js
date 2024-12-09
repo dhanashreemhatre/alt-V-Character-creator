0925ae58f8d5733208c4c2b06864ffb80101c2b8
dhanashreemhatre <mhatredhanashree682@gmail.com>
2024-12-09 16:03:46 +0530

    Added colorful eyes

--- utility/eyes.js
+++ utility/eyes.js
@@ -1,32 +1,65 @@
-const eyeColors = [
-    "black", // 0
-    "lightbluegreen", // 1
-    "darkblue", // 2
-    "brown", // 3
-    "darkerbrown", // 4
-    "lightbrown", // 5
-    "blue", // 6
-    "lightblue", // 7
-    "pink", // 8
-    "yellow", // 9
-    "purple", // 10
-    "black", // 11
-    "darkgreen", // 12
-    "lightbrown", // 13
-    "yellowblackpattern", // 14
-    "lightspiralpattern", // 15
-    "shinyred", // 16
-    "shinybluehalfred", // 17
-    "halfblackhalflightblue", // 18
-    "whiteredperimeter", // 19
-    "greensnake", // 20
-    "redsnake", // 21
-    "darkbluesnake", // 22
-    "darkyellow", // 23
-    "brightyellow", // 24
-    "allblack", // 25
-    "redsmallpupil", // 26
-    "devilblueblack", // 27
-    "whitesmallpupil", // 28
-    "glossedover", // 29
+// const eyeColors = [
+//     "black", // 0
+//     "lightbluegreen", // 1
+//     "darkblue", // 2
+//     "brown", // 3
+//     "darkerbrown", // 4
+//     "lightbrown", // 5
+//     "blue", // 6
+//     "lightblue", // 7
+//     "pink", // 8
+//     "yellow", // 9
+//     "purple", // 10
+//     "black", // 11
+//     "darkgreen", // 12
+//     "lightbrown", // 13
+//     "yellowblackpattern", // 14
+//     "lightspiralpattern", // 15
+//     "shinyred", // 16
+//     "shinybluehalfred", // 17
+//     "halfblackhalflightblue", // 18
+//     "whiteredperimeter", // 19
+//     "greensnake", // 20
+//     "redsnake", // 21
+//     "darkbluesnake", // 22
+//     "darkyellow", // 23
+//     "brightyellow", // 24
+//     "allblack", // 25
+//     "redsmallpupil", // 26
+//     "devilblueblack", // 27
+//     "whitesmallpupil", // 28
+//     "glossedover", // 29
+//   ]
+
+const eyeColors= [
+    "#000000", // Black
+    "#E0FFFF", // Light Blue Green
+    "#00008B", // Dark Blue
+    "#A52A2A", // Brown
+    "#5C4033", // Darker Brown
+    "#D2B48C", // Light Brown
+    "#0000FF", // Blue
+    "#ADD8E6", // Light Blue
+    "#FFC0CB", // Pink
+    "#FFFF00", // Yellow
+    "#800080", // Purple
+    "#000000", // Black
+    "#006400", // Dark Green
+    "#D2B48C", // Light Brown
+    "#FFFF00", // Yellow Black Pattern
+    "#F5F5F5", // Light Spiral Pattern
+    "#FF0000", // Shiny Red
+    "#FF4500", // Shiny Half Blue Half Red
+    "#4682B4", // Half Black Half Light Blue
+    "#FFFAFA", // White Red Perimeter
+    "#228B22", // Green Snake
+    "#FF0000", // Red Snake
+    "#191970", // Dark Blue Snake
+    "#DAA520", // Dark Yellow
+    "#FFFF00", // Bright Yellow
+    "#000000", // All Black
+    "#FF6347", // Red Small Pupil
+    "#1E90FF", // Devil Blue Black
+    "#FFFFFF", // White Small Pupil
+    "#696969", // Glossed Over
   ]
